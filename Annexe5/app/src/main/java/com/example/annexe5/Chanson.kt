8a3968eea84bd1c3d8bd13668e1086437faa1431
Alexiane Chartrand <alex.sp_6@yahoo.ca>
2025-10-01 10:44:23 -0400

    save anexe5

--- Annexe5/app/src/main/java/com/example/annexe5/Chanson.kt
+++
@@ -1,4 +0,0 @@
-package com.example.annexe5
-
-class Chanson (var titre : String, var artiste : String) {
-}
--- Annexe5/app/src/main/java/com/example/annexe5/MainActivity.kt
+++ Annexe5/app/src/main/java/com/example/annexe5/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.annexe5
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     lateinit var liste : ListView
+
+    var v: ArrayList<HashMap<String, Any>> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -26,12 +33,56 @@ class MainActivity : AppCompatActivity() {
 
         liste = findViewById(R.id.liste)
 
-        val adapt = SimpleAdapter(this, remplirArrayList(),
+        v = remplirArrayList()
+
+        val adapt = SimpleAdapter(this, v , //remplirArrayList(),
             R.layout.chanson, arrayOf("position", "nom","date", "image"),
             intArrayOf(R.id.textePosition, R.id.textNom, R.id.textDate, R.id.texteCover ))
         // liste --> ArrayList // maps --> hashmap ; chaque item représente un hashmap
 
-        liste.setAdapter(adapt)
+        //liste.setAdapter(adapt) // java
+        liste.adapter = adapt // kotlin
+
+
+        val ec = Ecouteur()
+
+        //liste.setOnClickListener(ec)
+        //liste.setOnItemClickListener(ec) // java
+        liste.onItemClickListener = ec // kotlin
+
+
+        // autre méthode avec lambda, moins avantageux car doit réutiliser les paramètres
+
+//        liste.setOnItemClickListener { _, view : View?, position: Int, _ ->
+//
+//            val linearlayout = view as LinearLayout
+//            val textview = linearlayout.findViewById<TextView>(R.id.textNom)
+//            Toast.makeText(this@MainActivity, textview.text.toString(), Toast.LENGTH_SHORT).show()
+//
+//        }
+
+    }
+
+    inner class Ecouteur : AdapterView.OnItemClickListener {
+
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+
+
+//            val selectedSong = v.get(position)
+//            val nomChanson = selectedSong["nom"] as String // kotlin style
+
+            var titre = v[position]["nom"].toString()
+
+            Toast.makeText(this@MainActivity, "Vous avez sélectionnée ${titre}", Toast.LENGTH_SHORT).show()
+
+            // on part du paramètre view
+//            val linearlayout = view as LinearLayout
+//            val textview = linearlayout.findViewById<TextView>(R.id.textNom)
+//
+//            Toast.makeText(this@MainActivity, textview.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 
@@ -54,7 +105,6 @@ class MainActivity : AppCompatActivity() {
         chanson["position"] = 3
         chanson["date"] = "22/03/86"
         chanson["image"] = R.drawable.touchme
-
 
         return listeDonnes
     }
