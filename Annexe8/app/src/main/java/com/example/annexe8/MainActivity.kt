57d3943369aec694ddd85617fc6e5a41978cc9a5
Alexiane Chartrand <alex.sp_6@yahoo.ca>
2025-11-10 15:26:10 -0500

    annexe8 ajout

---
+++ Annexe8/app/src/main/java/com/example/annexe8/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.annexe8
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationSet
+import android.view.animation.BounceInterpolator
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bouton : Button
+    lateinit var view : View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        bouton = findViewById(R.id.button)
+        view = findViewById(R.id.view)
+
+        var anim = ObjectAnimator.ofFloat(view, View.X, 1070f ) // bouton va se déplacer de x = 500px à x = 1070px
+        anim.duration = 2000
+        anim.interpolator = BounceInterpolator()
+        anim.repeatCount = ObjectAnimator.INFINITE
+        anim.repeatMode = ObjectAnimator.REVERSE
+
+        var anim2 = ObjectAnimator.ofFloat(view, View.ROTATION_X, 360f)
+        anim2.duration = 1000
+        anim2.interpolator = BounceInterpolator()
+
+        anim2.repeatCount = ObjectAnimator.INFINITE
+        anim2.repeatMode = ObjectAnimator.REVERSE
+
+        var animatorSet = AnimatorSet()
+        animatorSet.playTogether(anim, anim2)
+        //animatorSet.playSequentially(anim, anim2)
+
+        bouton.setOnClickListener{
+
+            animatorSet.start()
+//            anim.start() // démarrer l’animation
+//            anim2.start()
+
+        }
+
+
+    }
+
+
+}
---
+++ Annexe8/app/src/main/java/com/example/annexe8/MainActivity2.kt
@@ -0,0 +1,53 @@
+package com.example.annexe8
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.View.TRANSLATION_Y
+import android.view.animation.BounceInterpolator
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+
+    lateinit var barre : LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var init = true
+
+        barre = findViewById(R.id.barre)
+
+        var anim = ObjectAnimator.ofFloat(barre, View.Y, 150f)
+        anim.duration = 2000
+
+//        var anim2 = ObjectAnimator.ofFloat(barre, TRANSLATION_Y, 0f)
+//        anim2.reverse()
+
+
+        barre.setOnClickListener {
+
+            if (init) {
+
+                anim.start()
+                init = false
+            }
+            else {
+                anim.reverse()
+            }
+
+
+        }
+    }
+}
